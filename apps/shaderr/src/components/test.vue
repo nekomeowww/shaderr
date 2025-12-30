ea7e3af286fdc5d4beca6b955b68e85108d6da2e
Neko Ayaka <neko@ayaka.moe>
2025-12-30 12:52:00 +0800

    fix(shaderr): unused var

--- apps/shaderr/src/components/test.vue
+++ apps/shaderr/src/components/test.vue
@@ -1,19 +1,28 @@
 <script setup lang="ts">
-import { useTemplateRef, watch } from 'vue'
+import { useAsyncState } from '@vueuse/core'
+import { isWebGPUSupported } from 'gpuu/webgpu'
+import { useTemplateRef } from 'vue'
 
 const canvasRef = useTemplateRef<HTMLCanvasElement>('canvasRef')
 
-watch(canvasRef, () => {
-  if (!canvasRef.value) {
-    return
-  }
+function useGPUAdapter() {
+  const adapter = useAsyncState(() => navigator.gpu.requestAdapter(), null)
+  const supported = useAsyncState(() => isWebGPUSupported(), false)
 
-  const adapter = navigator.gpu.requestAdapter()
-}, { immediate: true })
+  return {
+    adapter,
+    supported,
+  }
+}
+
+const { adapter, supported } = useGPUAdapter()
 </script>
 
 <template>
   <div>
+    Is Ready: {{ adapter.isReady.value }}
+    <br>
+    Is Supported: {{ supported.state.value }}
     <canvas ref="canvasRef" width="512" height="512" />
   </div>
 </template>
